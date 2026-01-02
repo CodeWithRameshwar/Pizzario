3afcf059f7c8411f3d82b212a329d83e4dad4293
Code_With_Rameshwar <rameshwar.vidhate001@gmail.com>
2026-01-02 23:46:03 +0530

    Backend connected with MongoDB and base setup complete

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Pizza API Running...");
+});
+
+app.listen(5000, () => console.log("Server started on port 5000"));
